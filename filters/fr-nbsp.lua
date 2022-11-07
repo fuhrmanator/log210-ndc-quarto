f6a5be25141c10a720463567caf82993f6a64692
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-11-07 11:20:07 -0500

    Add case of hyperlinks for french punctuation spacing

--- filters/fr-nbsp.lua
+++ filters/fr-nbsp.lua
@@ -24,8 +24,8 @@ local function space_high_punctuation_and_quotes(inlines)
             inlines[i].text = string.gsub(inlines[i].text, "»", nbsp .. "»")
             inlines[i].text = string.gsub(inlines[i].text, "«", "«" .. nbsp)
         end
-        -- quotes, citation
-        if inlines[i+1] and (inlines[i].t == 'Quoted' or inlines[i].t == 'Cite') 
+        -- special cases where punctuation can follow
+        if inlines[i+1] and (inlines[i].t == 'Quoted' or inlines[i].t == 'Cite' or inlines[i].t == 'Link') 
             and inlines[i+1].t == 'Str' 
             and inlines[i+1].text:match(ascii_punctuation_pattern) then
                 inlines[i+1].text = nbsp .. inlines[i+1].text
