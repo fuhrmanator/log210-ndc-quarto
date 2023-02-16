8c9b147e6f0c67c314770c65c71ee0069dfbe245
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2023-02-15 22:33:18 -0500

    m-a-j filtre

--- filters/fr-nbsp.lua
+++ filters/fr-nbsp.lua
@@ -3,21 +3,21 @@
 --- Copyright: © 2022 Romain Lesur
 --- License: MIT – see LICENSE for details
 
-PANDOC_VERSION:must_be_at_least '2.9.2'
-
-thin_space_pattern_ascii = '[;!%?]'
-wide_space_pattern_ascii = '[%%:]'
-all_space_pattern_ascii = '[;!%?%%:]'
-thin_nbsp = '\u{202f}'
-nbsp = '\u{00A0}'
-
 --- Copyright: © 2022 Christopher Fuhrman
 --- License: MIT – see LICENSE for details
 
+PANDOC_VERSION:must_be_at_least '2.9.2'
+
+local THIN_SPACE_PATTERN_ASCII = '[;!%?]'
+local WIDE_SPACE_PATTERN_ASCII = '[%%:]'
+local ALL_SPACE_PATTERN_ASCII = '[;!%?%%:]'
+local THIN_NBSP = '\u{202f}'
+local NBSP = '\u{00A0}'
+
 local function determine_space_type(character)
-    local space = nbsp
-    if (string.match(character, thin_space_pattern_ascii)) then
-        space = thin_nbsp
+    local space = NBSP
+    if (string.match(character, THIN_SPACE_PATTERN_ASCII)) then
+        space = THIN_NBSP
     end
     return space
 end
@@ -27,54 +27,42 @@ local function insert_nonbreaking_space_before_last_char(text)
     return text:sub(1, -2) .. space .. text:sub( -1)
 end
 
+local function string_already_has_nbsp(text)
+    return string.find(text, THIN_NBSP)
+        or string.find(text, NBSP)
+end
+
 --- add non-breaking spaces according to high punctuation rules, similar to babel-french
 local function space_high_punctuation_and_quotes(inlines)
     local i = 1
     -- local already_spaced = false
     while inlines[i] do
         -- print(i, '//', inlines[i], '//', i+1,  '//', inlines[i+1], '//')
-        -- punctuation after parentheses, e.g., ([Markup](https://example.com));
-        if inlines[i + 1] and
-            inlines[i + 1].t == 'Str' and
-            inlines[i + 1].text == ');' and
-            (string.match(inlines[i + 1].text:sub( -1), all_space_pattern_ascii)) then
-            -- print(i, "fixing:", inlines[i+1])
-            -- inlines[i + 1].text = inlines[i + 1].text:sub(1, -2) ..
-            --     thin_nbsp .. inlines[i + 1].text:sub( -1)
-            inlines[i + 1].text = insert_nonbreaking_space_before_last_char(inlines[i + 1].text)
-            -- already_spaced = true
-        else
-            -- ignore if nbsp is already in string
-            if inlines[i].t == 'Str' and not (string.find(inlines[i].text, thin_nbsp) or string.find(inlines[i].text, nbsp)) then
-                if string.len(inlines[i].text) > 1 and string.match(inlines[i].text:sub( -1), all_space_pattern_ascii) then
-                    -- print(inlines[i].text:sub(-1))
-                    -- insert thin_nbsp before last char
-                    -- print(i, 'fixing (2):', inlines[i])
-                    inlines[i].text = insert_nonbreaking_space_before_last_char(
-                        inlines[i].text)
-                    -- already_spaced = true
-                end
-                -- unicode is a problem in patterns, so we just brute force it?
-                inlines[i].text = string.gsub(inlines[i].text, "€", nbsp .. "€")
-                inlines[i].text = string.gsub(inlines[i].text, "»", nbsp .. "»")
-                inlines[i].text = string.gsub(inlines[i].text, "«", "«" .. nbsp)
+        if inlines[i].t == 'Str' and not string_already_has_nbsp(inlines[i].text) then
+            if string.len(inlines[i].text) > 1 and string.match(inlines[i].text:sub( -1), ALL_SPACE_PATTERN_ASCII) then
+                -- print(inlines[i].text:sub(-1))
+                -- print(i, 'fixing (2):', inlines[i])
+                inlines[i].text = insert_nonbreaking_space_before_last_char(
+                    inlines[i].text)
             end
+            -- unicode is a problem in patterns, so we just brute force it?
+            inlines[i].text = string.gsub(inlines[i].text, "€", NBSP .. "€")
+            inlines[i].text = string.gsub(inlines[i].text, "»", NBSP .. "»")
+            inlines[i].text = string.gsub(inlines[i].text, "«", "«" .. NBSP)
         end
 
         --
         -- special cases where punctuation can follow
         --
 
-        if inlines[i + 1] and inlines[i + 1].t == 'Str' and not string.find(inlines[i + 1].text, thin_nbsp) -- didn't already insert thin_nbsp
+        if inlines[i + 1] and inlines[i + 1].t == 'Str' and not string.find(inlines[i + 1].text, THIN_NBSP) -- didn't already insert THIN_NBSP
             and (inlines[i].t == 'Quoted' or inlines[i].t == 'Cite' or inlines[i].t == 'Link' or inlines[i].t == 'Emph' or inlines[i].t == 'Strong' or inlines[i].t == 'Strikeout' or inlines[i].t == 'Code' or inlines[i].t == 'RawInline')
-            and string.match(inlines[i + 1].text:sub( -1), all_space_pattern_ascii) then
+            and string.match(inlines[i + 1].text:sub( -1), ALL_SPACE_PATTERN_ASCII) then
             if (string.len(inlines[i + 1].text) == 1) then
                 -- print(i, 'fixing (3):', inlines[i+1])
                 inlines[i + 1].text = determine_space_type(inlines[i + 1].text) .. inlines[i + 1].text
             else
                 -- print(i, 'fixing (4) -- should be parens:', inlines[i+1])
-                -- inlines[i + 1].text = inlines[i + 1].text:sub(1, -2) ..
-                --     thin_nbsp .. inlines[i + 1].text:sub( -1)
                 inlines[i + 1].text = insert_nonbreaking_space_before_last_char(inlines[i + 1].text)
             end
             -- skip the item we just spaced
@@ -83,12 +71,12 @@ local function space_high_punctuation_and_quotes(inlines)
 
         --- special case where string is terminated by parentheses, e.g., "Bonjour!)"
 
-        if inlines[i].t == 'Str' and string.find(inlines[i].text, '.*' .. all_space_pattern_ascii .. '%)') then
+        if inlines[i].t == 'Str' and string.find(inlines[i].text, '.*' .. ALL_SPACE_PATTERN_ASCII .. '%)') then
             -- print("Found: ", inlines[i].text)
             -- capture what's in the parens
             _, _, inside = string.find(inlines[i].text,
-                '(.*' .. all_space_pattern_ascii .. ')%)')
-            inside = inside:sub(1, -2) .. thin_nbsp .. inside:sub( -1)
+                '(.*' .. ALL_SPACE_PATTERN_ASCII .. ')%)')
+            inside = inside:sub(1, -2) .. THIN_NBSP .. inside:sub( -1)
             inlines[i].text = inside .. ')'
         end
         i = i + 1
